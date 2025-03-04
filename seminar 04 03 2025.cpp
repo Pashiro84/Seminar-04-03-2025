315be5d9618f7bbfad99f322feeb2e16c888c683
Pashiro84 <ShirokovPavel8484@gmail.com>
2025-03-04 10:40:14 +0700

    1

--- seminar 04 03 2025.cpp
+++ seminar 04 03 2025.cpp
@@ -5,9 +5,26 @@
 
 using namespace std;
 
+// Структура для динамической строки
+struct DynamicString {
+    char* data; // Указатель на массив символов
+    int length; // Длина строки
+};
+
+// Функция для взятия из строки str подстроки длиной len начиная с позиции pos. Необходимо проверить допустимы ли значения pos и len.
+
+DynamicString substring(const DynamicString& str, size_t pos, size_t len) 
+{
+   // Напишите код функции
+    if (str.lenght <= len + pos)
+    {
+
+    }
+}
+
 int main()
 {
-
+    DynamicString S = ("SFU", 3);
     
     return 0;
 }
