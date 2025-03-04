d6c6ec9d0ae290d38ed405ea822c0bfcbe42359d
Pashiro84 <ShirokovPavel8484@gmail.com>
2025-03-04 11:21:06 +0700

    4

--- seminar 04 03 2025.cpp
+++ seminar 04 03 2025.cpp
@@ -17,6 +17,8 @@ struct DynamicString {
 
 DynamicString substring(const DynamicString& str, size_t pos, size_t len) 
 {
+    char j[max_lines] = "\0";
+    DynamicString V = {j, 0};
     char N[max_lines];
     // Напишите код функции
     if (str.length >= len + pos)
@@ -24,29 +26,29 @@ DynamicString substring(const DynamicString& str, size_t pos, size_t len)
         int i = pos;
         for (; i < pos + len; i++)
         {
-            N[i] = str.data[i];
-            cout << N << endl;
+            N[i-pos] = str.data[i];
         }
-        N[i] = '\0';
-        cout << N << endl;
+        N[i-pos] = '\0';;
         int lent = len;
-        DynamicString V = {N, lent };
-        return V;
+        V.data = N;
+        V.length = len;
     }
     else 
     {
         char None[max_lines] = "None";
         int i = 4;
-        DynamicString V = {None, i };
-        return V;
+        V.data = None;
+        V.length = i;
     }
+    return V;
 }
 
 int main()
 {
     char Sl[max_lines] = "SFU";
     DynamicString S = { Sl, 3};
-    DynamicString N = substring(S, 1, 4);
+    cout << S.data << " " << S.length << endl;
+    DynamicString N = substring(S, 1, 2);
     cout << N.data << " " << N.length << endl;
     return 0;
 }
