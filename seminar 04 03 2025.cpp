c3b2f176cd35fef032c12afde89293ddd7517cef
Pashiro84 <ShirokovPavel8484@gmail.com>
2025-03-04 10:55:27 +0700

    2

--- seminar 04 03 2025.cpp
+++ seminar 04 03 2025.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+const int max_lines=50;
+
 // Структура для динамической строки
 struct DynamicString {
     char* data; // Указатель на массив символов
@@ -15,16 +17,27 @@ struct DynamicString {
 
 DynamicString substring(const DynamicString& str, size_t pos, size_t len) 
 {
-   // Напишите код функции
-    if (str.lenght <= len + pos)
+    char N[max_lines];
+    // Напишите код функции
+    if (str.length >= len + pos)
     {
-
+        int i = pos;
+        for (; i < pos + len; i++)
+        {
+            N[i] = str.data[i];
+        }
+        N[i] = '\0';
+        int lent = len;
+        DynamicString V = {N, lent };
+        return V;
     }
 }
 
 int main()
 {
-    DynamicString S = ("SFU", 3);
-    
+    char Sl[max_lines] = "SFU";
+    DynamicString S = { Sl, 3};
+    DynamicString N = substring(S, 1, 1);
+    cout << N.data << " " << N.length << endl;
     return 0;
 }
